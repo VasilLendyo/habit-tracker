2122cc2d1178f98ae2c4d5ac0d6d7f2e9fdb8445
Vasyl <vasyllendyo91@gmail.com>
2026-07-09 12:01:32 +0300

    added comments

--- script.js
+++ script.js
@@ -1,55 +1,77 @@
+// Знаходжу кнопки в HTML, щоб потім змусити її працювати
 const habitInput = document.getElementById('habit-input');
 const addBtn = document.getElementById('add-btn');
 const habitList = document.getElementById('habit-list');
 
+//Створення масиву
 let habits = [];
 
+//відмальвка в браузері
 function renderHabits(){
+    //Очищення після кожної звички
     habitList.innerHTML = '';
 
+    //Перебирання нашого масиву
     habits.forEach((habit, index) => {
+        // Створення нового елемента списку в пам'яті
         const li = document.createElement('li');
+        //присвоєння класу елементу списку для підключення CSS-стилів
         li.className = 'habit-item';
         
+        // Створення HTML-елементу span для тексту звички
         const textSpan = document.createElement('span');
-        textSpan.textContent = habit.text;
-        textSpan.className = 'habit-text';
+            //внесення тексту з масиву в цей span і присвоєння класу для стилізації
+            textSpan.textContent = habit.text;
+            textSpan.className = 'habit-text';
 
+        //Перевірка стану об'єкта (виконано чи ні) і додавання класу для перекреслення тексту
         if(habit.done) {
             textSpan.classList.add('completed');
         }
 
+        //Додаємо подію кліку на текст звички для зміни статусу (виконано/ні) та оновлення екрану
         textSpan.addEventListener('click', () => {
-            habit.done = !habit.done;
-            renderHabits();
+            habit.done = !habit.done; // перемикаємо статус на протилежний
+            renderHabits(); // перемальовуємо візуал з новими стилями
         });
 
+
+        // Вкладаємо тег span з текстом всередину нашого елемента списку li
         li.appendChild(textSpan);
 
+        //створення кнопки для видалення елементу
         const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = '❌';
-        deleteBtn.className = 'delete-btn';
+        deleteBtn.textContent = '❌'; //вміст кнопки
+        deleteBtn.className = 'delete-btn'; //присвоєння класу
 
+        // Навішуємо подію кліку на кнопку видалення
         deleteBtn.addEventListener('click', () => {
-            habits.splice(index, 1);
-            renderHabits();
+            habits.splice(index, 1); // Видаляємо з масиву 1 елемент за його порядковим номером (індексом)
+            renderHabits(); // Перемальовуємо список у браузері вже без цього елемента
         });
 
+        // Вкладаємо кнопку видалення всередину нашого елемента списку li
         li.appendChild(deleteBtn);
+        // Вкладаємо елементи списку у сам список
         habitList.appendChild(li);
     });
 }
 
+// Навішуємо подію кліку на кнопку "Додати"
 addBtn.addEventListener('click', () => {
+    // Отримуємо текст з інпуту та очищаємо його від зайвих пробілів по краях
     const text = habitInput.value.trim();
 
+    // Перевірка на введення пустого рядка
     if(text === '') {
-        alert('Введіть назву звички!');
-        return;
+        alert('Введіть назву звички!'); // Виводимо попередження
+        return; // Зупиняємо виконання функції, щоб порожня звичка не додалася
     }
 
+    // Створюємо об'єкт нової звички та додаємо його в кінець нашого масиву
     habits.push({text: text, done: false});
 
+    // Очищаємо поле
     habitInput.value = '';
-    renderHabits();
+    renderHabits(); //перемальвуємо візуал в браузері з новою звичкою
 })
